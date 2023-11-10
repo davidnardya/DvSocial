09f200da85d07f0a7f2766518134acf6af7cc7f0
davidnardyayit <david-n@yit.co.il>
2023-11-10 15:47:02 +0200

    Added a splash after post button clicked

--- app/src/main/java/com/davidnardya/dvsocial/navigation/screens/PostScreen.kt
+++ app/src/main/java/com/davidnardya/dvsocial/navigation/screens/PostScreen.kt
@@ -2,12 +2,16 @@ package com.davidnardya.dvsocial.navigation.screens
 
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -17,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
@@ -49,12 +54,17 @@ fun PostScreen(
         mutableStateOf<Uri?>(null)
     }
     val screenHeight = LocalConfiguration.current.screenHeightDp
-    if (showSpinner) {
-        SplashScreen(feedViewModel)
-    }
+
     Column(
         modifier = Modifier.padding(6.dp)
     ) {
+        if (showSpinner) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
+            )
+        }
         Button(
             modifier = Modifier
                 .fillMaxWidth()
@@ -86,27 +96,16 @@ fun PostScreen(
                         }
                         delay(3000)
                         showSpinner = false
-//                        navController.navigate(Screen.Feed.route) {
-//                            popUpTo(Screen.Post.route) {
-//                                inclusive = true
-//                            }
-//                            popUpTo(Screen.Splash.route) {
-//                                inclusive = true
-//                            }
-//                        }
-//                        val posts = currentUser?.posts?.toMutableList()
-//                        posts?.add(UserPost(
-//                            downloadUri?.toString(),
-//                            postText,
-//                            username = currentUser.username
-//                        ))
-//                        loginViewModel.currentUser.value = DvUser(
-//                            currentUser?.id,
-//                            currentUser?.username,
-//                            currentUser?.password,
-//                            posts,
-//                            currentUser?.notifications
-//                        )
+                        selectedImageUri = null
+                        postText = ""
+                        navController.navigate(Screen.Feed.route) {
+                            popUpTo(Screen.Post.route) {
+                                inclusive = true
+                            }
+                            popUpTo(Screen.Splash.route) {
+                                inclusive = true
+                            }
+                        }
                     }
                 }
             }
@@ -118,9 +117,9 @@ fun PostScreen(
                 .heightIn(0.dp, (screenHeight - buttonHeight).dp)
                 .fillMaxWidth(),
             value = postText,
-            placeholder = { Text(text = "Write something here")},
+            placeholder = { Text(text = "Write something here") },
             onValueChange = {
-                if(it.length <= Constants.MAX_COMMENT_LENGTH) {
+                if (it.length <= Constants.MAX_COMMENT_LENGTH) {
                     postText = it
                 }
             }
@@ -133,7 +132,7 @@ fun PostScreen(
         ) {
             Text(text = "Add a new picture")
         }
-        if(selectedImageUri?.path != null && selectedImageUri?.path != "") {
+        if (selectedImageUri?.path != null && selectedImageUri?.path != "") {
             Image(
                 modifier = Modifier
                     .fillMaxSize(),
